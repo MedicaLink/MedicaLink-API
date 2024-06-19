dcded6d24ef8a0c7304bf9a9858008c2ba0e0cba
Nisala Dulanaka <dulanakanisala654@gmail.com>
2024-06-19 22:13:15 +0530

    Created the patient controller with search and patient profile view functionality

---
+++ API/Models/FormModels/PatientSearchModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.FormModels
+{
+    public class PatientSearchModel
+    {
+        public string? Query { get; set; }
+
+        public string Type { get; set; } = "NIC";
+    }
+}
